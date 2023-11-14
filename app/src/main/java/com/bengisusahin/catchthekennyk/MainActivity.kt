4f7189a9c01c3e5309082712c7132f11cc68c5da
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-14 20:55:54 +0300

    increased the score

--- app/src/main/java/com/bengisusahin/catchthekennyk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/catchthekennyk/MainActivity.kt
@@ -2,14 +2,21 @@ package com.bengisusahin.catchthekennyk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bengisusahin.catchthekennyk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun increaseScore(view: View){
+        score = score +1
+        binding.scoreText.text = "Score: ${score}"
     }
 }
