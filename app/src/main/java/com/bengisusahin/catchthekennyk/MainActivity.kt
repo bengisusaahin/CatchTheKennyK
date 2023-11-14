c96389899307fa13e1e58d766cfc0bdbaf1709a0
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-14 21:51:03 +0300

    Moved the kenny

--- app/src/main/java/com/bengisusahin/catchthekennyk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/catchthekennyk/MainActivity.kt
@@ -2,6 +2,8 @@ package com.bengisusahin.catchthekennyk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageView
 import com.bengisusahin.catchthekennyk.databinding.ActivityMainBinding
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var score = 0
     var imageArray = ArrayList<ImageView>()
+    var runnable = Runnable {} //interface oldugu icin ve object diyerek tanimladigimiz icin her yerden ulasabilmek icin burda baslatmadik
+    var handler = Handler(Looper.getMainLooper()) //object oldugu icin burda tanimlayabildik
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -33,12 +38,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun hideImages(){
-        for (image in imageArray){
-            image.visibility = View.INVISIBLE
+
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+                val random = Random()
+                val randomIndex = random.nextInt(9)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 500)
+            }
         }
-        val random = Random()
-        val randomIndex = random.nextInt(9)
-        imageArray[randomIndex].visibility = View.VISIBLE
+        handler.post(runnable)
     }
 
     fun increaseScore(view: View){
